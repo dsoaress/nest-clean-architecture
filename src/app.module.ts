0d36cb6e49b11f32d376c0ef9e9ec3c8aba570aa
Daniel Soares <daniel.soares@me.com>
2022-05-27 19:17:21 -0300

    feat: implements user controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Logger, Module } from '@nestjs/common'
 
+import { UserRepository } from '@/core/repositories/user.repository'
+import { UsersCacheMemoryRepository } from '@/data/cache-memory/users-cache-memory.repository'
+import { UsersControllers } from '@/presentation/users.controller'
+import { CreateUserUseCase } from '@/use-cases/create-user.usecase'
+import { GetAllUsersUseCase } from '@/use-cases/get-all-users.usecase'
+
 @Module({
-  providers: [Logger]
+  controllers: [UsersControllers],
+  providers: [
+    {
+      provide: UserRepository,
+      useClass: UsersCacheMemoryRepository
+    },
+    CreateUserUseCase,
+    GetAllUsersUseCase,
+    Logger
+  ]
 })
 export class AppModule {}
